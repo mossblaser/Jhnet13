2cc8865d7b9bfaaafe4990605669d6d439448255
Jonathan Heathcote <mail@jhnet.co.uk>
2013-05-30 15:43:26 +0100

    Updated jhnet.py to have local misc.

--- site/jhnet.py
+++ site/jhnet.py
@@ -99,7 +99,7 @@ articles = publication.Publications( "/articles"
 
 # The misc directory (with directory browser)
 misc = static.StaticBrowseableDir( "/misc"
-                                 , "/home/jonathan"
+                                 , "./misc"
                                  , misc_template
                                  , root_path = "/"
                                  , site_menu = site_menu
