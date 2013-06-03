b98f2446ee8c4b4a246c71f56165d1b265997d46
Jonathan Heathcote <mail@jhnet.co.uk>
2013-06-03 10:11:37 +0100

    Removed attempted fcgi detection.

--- index.py
+++ index.py
@@ -10,10 +10,8 @@ from jhnet.app import app
 # redirect.
 os.environ["REAL_SCRIPT_NAME"] = ""
 
-# A bizarre hack which makes FastCGI work in web.py. Detect that we're running
-# under fcgi by checking for the presence of this environment variable
-if "SERVER_NAME" in os.environ:
-	web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
+# A bizarre hack which makes FastCGI work in web.py.
+web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
 
 # Start the fcgi client
 if __name__=="__main__":
